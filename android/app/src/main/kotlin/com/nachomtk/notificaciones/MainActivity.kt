e95dbb3711aec73f84706c4be1063ee271776354
Nacho <44799327+nachom96@users.noreply.github.com>
2022-09-05 11:55:08 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/nachomtk/notificaciones/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nachomtk.notificaciones
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:notificaciones/screens/home_screen.dart';
+import 'package:notificaciones/screens/message_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => HomeScreen(),
+        'message': ( _ ) => MessageScreen()
+      },
+    );
+  }
+}

8fb3d29db49fd9e25f21142c1b1bc76ca3c01071
Nacho <44799327+nachom96@users.noreply.github.com>
2022-09-13 16:55:52 +0200

    Añadido recibir notificación

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -9,12 +9,29 @@ class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
 
+  static Future _backgroundHandler ( RemoteMessage message ) async{
+    print('onBackground Handler ${message.messageId}');
+  }
+
+  static Future _onMessageHandler ( RemoteMessage message ) async{
+    print('onMessage Handler ${message.messageId}');
+  }
+
+  static Future _onMessageOpenApp ( RemoteMessage message ) async{
+    print('onMessageOpenApp Handler ${message.messageId}');
+  }
+
   static Future initializeApp() async{
 
     // Push Notifications
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
     print('token = $token');
+
+    // Handlers
+    FirebaseMessaging.onBackgroundMessage( _backgroundHandler );
+    FirebaseMessaging.onMessage.listen( _onMessageHandler );
+    FirebaseMessaging.onMessageOpenedApp.listen( _onMessageOpenApp );
 
     //Local Noticications 
       
